540c15308c9288caa012d97f819c450137e91065
Alice <alice.gruby@gmail.com>
2026-02-14 16:54:50 +0100

    Update accompagnements, mentions legales, media query, confidentialite

--- assets/js/pages.js
+++ assets/js/pages.js
@@ -1,3 +1,4 @@
+/* MOUVEMENT IMAGES ET BOUTONS */
 window.addEventListener('load', () => {
     // Images
     const images = document.querySelectorAll('.animate-img, .animate-img-left');
@@ -10,5 +11,22 @@ window.addEventListener('load', () => {
     buttons.forEach((btn, index) => {
         setTimeout(() => btn.classList.add('animate-btn-active'), 800 + (200 * index));
     });
+
+    // Blocs Mes accompagnements
+    const blocks = document.querySelectorAll('.animate-block');
+    blocks.forEach((block, index) => {
+        setTimeout(() => block.classList.add('animate-block-active'), 500 + (300 * index));
+    });
 });
 
+/* GALERIE D'IMAGES ACCOMPAGNEMENTS */
+document.querySelectorAll(".carousel").forEach(carousel => {
+  const images = carousel.querySelectorAll("img");
+  let index = 0;
+
+  setInterval(() => {
+    images[index].classList.remove("active");
+    index = (index + 1) % images.length;
+    images[index].classList.add("active");
+  }, 4000);
+});

8cf69476dca8385b1d4f02dba48cdf1947a8a35d
Alice <alice.gruby@gmail.com>
2026-02-18 09:57:29 +0100

    Update pour ne pas voir index.html dans l'url

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -6,3 +6,8 @@ burger.addEventListener('click', () => {
   navUl.classList.toggle('show');
   burger.classList.toggle('toggle');
 });
+
+// Retirer index.html
+if (window.location.pathname.endsWith("index.html")) {
+  window.location.replace(window.location.pathname.replace("index.html", ""));
+}
